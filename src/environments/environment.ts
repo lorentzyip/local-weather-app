de06a3e4967f9d7daa00b59b5e592a0b2e3a05e5
Lorentz Yip <lorentz.yip@gmail.com>
2018-12-05 16:37:23 +0800

    Null guarding current-weather component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '6c87706315903015f3d0f5fac54aa667',
+  baseUrl: 'http://',
 };
 
 /*
